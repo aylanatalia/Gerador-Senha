17aac207a265a5759a7cfa9ce2efb247164e73e2
Ayla Natalia Martins Ferreira <aylantalia08@hotmail.com>
2024-09-03 20:46:38 -0300

    input range faltando cor da parte preenchida

--- js/range.js
+++ js/range.js
@@ -1,14 +1,8 @@
 const barra = document.getElementById('numero-caracteres');
 const valorBarra = document.getElementById('valor-selecionado');
 
-
-
 barra.addEventListener('input', () => {
     //para que o span imprima o valor selecionado na barra
     valorBarra.textContent = barra.value;
-
-    //para mudar a cor da barra
-    const thumbPosition = (barra.value / barra.max) * 100;
-    barra.style.setProperty('--thumb-position', `${thumbPosition}%`)
 })
 
