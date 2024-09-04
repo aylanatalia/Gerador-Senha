a8cb90f918e216622867409f772e06bb802bd26e
Ayla Natalia Martins Ferreira <aylantalia08@hotmail.com>
2024-09-04 08:51:58 -0300

    estilizacao da minha barra do input

--- js/range.js
+++ js/range.js
@@ -6,3 +6,9 @@ barra.addEventListener('input', () => {
     valorBarra.textContent = barra.value;
 })
 
+
+//aplica a estilização ao componente barra 
+barra.oninput = function(){
+    var valor = (this.value - this.min) / (this.max - this.min) * 100;
+    this.style.background = `linear-gradient(to right, #FFFFFF 0%, #FFFFFF ${valor}%, #2C1746 ${valor}%, #2C1746 100%)`;
+}
